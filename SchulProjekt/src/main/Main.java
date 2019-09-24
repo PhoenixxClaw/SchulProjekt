1629f7f28241d116d5149f6759585eda314d643b
Leon Lima Cardoso <bitbucket@senpaikeulekun.de>
2019-09-24 16:00:24 +0200

    <24.09.2019>
    - BenutzerDAO
    - LoginMaske
    - Benutzer-Entity

--- SchulProjekt/src/main/Main.java
+++ SchulProjekt/src/main/Main.java
@@ -1,10 +1,36 @@
 package main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		try {
+			Stage stage = new Stage();
+			Parent start;
+			FXMLLoader fxmlLoader = new FXMLLoader(
+					getClass().getResource("/gui/LOGIN_gui.fxml"));
+			start = (Parent) fxmlLoader.load();
+			stage.setScene(new Scene(start));
+			stage.setResizable(false);
+			stage.setTitle("Login");
+			stage.show();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
 	}
 
 }
